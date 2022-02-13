8899285aefe5ed58e96f7ebb0b79f1af502cb2d3
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-02-13 17:39:45 +0100

    cmake

---
+++ build/CMakeFiles/DeepLearning.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeepLearning.dir/main.o"
+  "CMakeFiles/DeepLearning.dir/math.o"
+  "DeepLearning"
+  "DeepLearning.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DeepLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

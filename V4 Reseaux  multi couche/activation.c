7272e293ec8d8e02f3e66afb71a9fc7041d4f30e
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-04-18 15:14:17 +0200

    up

--- V4 Reseaux  multi couche/activation.c
+++ V4 Reseaux  multi couche/activation.c
@@ -1,5 +1,12 @@
 #include "activation.h"
 #include <math.h>
+//calcule la fonction d'activation sigmoide
 DATATYPE sigmoide(DATATYPE x){
     return 1/(1+exp(-x));
+}
+
+
+//calcule la fonction d'activation Heaviside
+DATATYPE Heaviside(DATATYPE x)
+    return 
 }

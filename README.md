d9631f0d175d211a9cd436e2c1b9d9378e7ee784
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-04-30 22:43:35 +0200

    clean

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # DeapLearning
 Projet autodidacte etudiant sur le machine-learning et le deap-learning
+
+V3: regression lineaire modéle un couche lineaire
+
+V4: regression polynomiale mulicouche multineurones
+
+# diferent parametres sont réglable et son défint dans le fichier setting.h
+(nombre de neurone,couche,data,paramétre,iteration;fonction d'activation,type de donné,bornes)
+
+# compilation en mode debug
+-définir debug dans setting
+-instalé les paquet cmake et valgrind si besoin
+-dans le ficher build:
+    vidé le dossier build si besoin
+    cmake -DTYPE=Debug ..
+    make
+    valgrind ./Deeplearning
+
+# compilation normal
+-commenté le deffine debug dans setting
+-instalé le paquet cmake
+-dans le ficher build:
+    vidé le dossier build si besoin
+    cmake
+    make
+    ./Deeplearning
--- V4 Reseaux  multi couche/setting.h
+++ V4 Reseaux  multi couche/setting.h
@@ -1,6 +1,6 @@
 #define DEBUG //à definir si on veux printf les message de debug attention à la saturation du shell
 
-#define NBPARAM 2 //nombre de parametre ici x et y
+#define NBPARAM 2 //nombre de parametre d'entrée du modéle ici x1 et x2
 #define NBDATA 4 //nombre de donné traité 
 #define LEARNINGRATE 2 //pas d'aprentissage
 #define NBITER 10 //nombre d'iteration
@@ -9,8 +9,8 @@
 #define BORNEMIN 10 //borne max du dataset generé
 #define DATATYPE float //type de donné traité aprés normalisation float ou double
 #define EPSILLONE FLT_MIN //Borne min du type de donée traité
-#define FONCTION(x) (2*x+10) //équoition du modéle suposé inconus
-#define CRITERE(x,y) (y>FONCTION(x)) //critére de teste
+#define FONCTION(x1) (2*x1+10) //équoition du modéle suposé inconus (frontierre de décision)
+#define CRITERE(x1,x2) (x2>FONCTION(x1)) //critére de teste
 #define NBCOUCHE 4 //nombre de couche du reseaux
 #define NBENTREE {NBPARAM,3,2,3,1} //nombre de neurone par couche / nombre d'entré par neurone
 #define FCTACTIVATION {sigmoide,sigmoide,sigmoide,sigmoide} //fonction d'activation par couche

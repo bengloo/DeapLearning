794b1b905c889c6a6d7972d7e5b79dfd3ccac882
Benjamin Engloo <engloo.benjamin@gmail.com>
2022-05-29 14:30:59 +0200

    up

--- Sci lab/bengloo.sce
+++ Sci lab/bengloo.sce
@@ -1,3 +1,85 @@
-x = [0 1/3 2/3 0 1/3 2/3 0 1/3 2/3; 0 0 0 1/3 1/3 1/3 2/3 2/3 2/3];
-matrix(x, 2, 9)
+format("v",10);
+nbdata=9
+//NBDATA=2
+n0=2//nbentre(0)
+n1=1//nbentre(1)=nbneurone(0)
+n2=1//nbentre(2)=nbneurone(1)
+a=0.1 //learning rate
+
+X = [0 1/3 2/3 0 1/3 2/3 0 1/3 2/3;0 0 0 1/3 1/3 1/3 2/3 2/3 2/3];X=matrix(X, n0, nbdata)
+
+//initialisation
+    //c0 N0
+        A0 = [0 0 0 0 0 0 0 0 0];A0=matrix(A0, n1, nbdata)
+        dz0 = [0 0 0 0 0 0 0 0 0];dz0=matrix(dz0, n1, nbdata)
+        w0 = [0; 1/2];w0=matrix(w0, n1, n0)
+        dw0 = [0 0];dw0=matrix(dw0, n1, n0)
+        b0 = [1];b0=matrix(b0,n1,1)
+        db0 = [0];db0=matrix(db0,n1,1)
+    //C1 N0
+        A1 = [0 0 0 0 0 0 0 0 0];A1=matrix(A1,n2,nbdata)
+        dz1 = [0 0 0 0 0 0 0 0 0];dz1=matrix(dz1,n2,nbdata)
+        w1 = [0];w1=matrix(w1,n2,n1)
+        dw1 = [0];dw1=matrix(dw1,n2,n1)
+        b1 = [1];b0=matrix(b0,n2,1)
+        db1 = [0];db1=matrix(db1,n2,1)   
+    Y = [0 0 0 1 0 0 1 1 0];matrix(Y, 1, nbdata)
+//iteration 0
+    //forward_propagation
+        A0=1./(1+exp(-(w0*X+b0)))
+        A1=1./(1+exp(-(w1*A0+b0)))
+    //back_propagation
+        dz1=A1-Y
+        dw1=(1/nbdata).*dz1*(A0')
+        db1=(1/nbdata).*sum(dz1)
+        
+/*        dz0=(w1')*dz1.*(A0.*(1-A0))
+        dw0=(1/nbdata).*dz0*(X')
+        db0=(1/nbdata).*sum(dz0)    
+    //update
+        w0=w0-a*dw0
+        b0=b0-a*db0
+        w1=w1-a*dw1
+        b1=b1-a*db1
+//iteration 1
+    //forward_propagation
+        A0=1./(1+exp(-(w0*X+b0)))
+        A1=1./(1+exp(-(w1*A0+b0)))
+    //back_propagation
+        dz1=A1-Y
+        dw1=(1/nbdata).*dz1*(A0')
+        db1=(1/nbdata).*sum(dz1)
+        
+        dz0=(w1')*dz1.*(A0.*(1-A0))
+        dw0=(1/nbdata).*dz0*(X')
+        db0=(1/nbdata).*sum(dz0)    
+    //update
+        w0=w0-a*dw0
+        b0=b0-a*db0
+        w1=w1-a*dw1
+        b1=b1-a*db1
+//iteration 2
+    //forward_propagation
+        A0=1./(1+exp(-(w0*X+b0)))
+        A1=1./(1+exp(-(w1*A0+b0)))
+    //back_propagation
+        dz1=A1-Y
+        dw1=(1/nbdata).*dz1*(A0')
+        db1=(1/nbdata).*sum(dz1)
+        
+        dz0=(w1')*dz1.*(A0.*(1-A0))
+        dw0=(1/nbdata).*dz0*(X')
+        db0=(1/nbdata).*sum(dz0)    
+    //update
+        w0=w0-a*dw0
+        b0=b0-a*db0
+        w1=w1-a*dw1
+        b1=b1-a*db1
+*/
+
+
+
+
+
+
 
--- V4 Reseaux  multi couche/setting.h
+++ V4 Reseaux  multi couche/setting.h
@@ -1,9 +1,12 @@
 
 #define SETINGVERSION 1
-//0:settings reseaux 1 couche 1 neurone regression d'un systéme lineaire 
+//0:settings reseaux 1 couche 1 neurone regression d'un systéme lineaire DataSet complet beaucoup d'iterations
     //https://playground.tensorflow.org/#activation=sigmoid&batchSize=30&dataset=circle&regDataset=reg-plane&learningRate=0.1&regularizationRate=0&noise=0&networkShape=1&seed=0.12746&showTestData=false&discretize=false&percTrainData=90&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=regression&initZero=false&hideText=false
-//autre:settings reseaux 2 couche 1 neurone regression d'un systéme lineaire 
+//1:settings reseaux 2 couche 1 neurone regression d'un systéme lineaire Dataset de test peux d'iteration
+//2:settings reseaux 2 couche 1 neurone regression d'un systéme lineaire Dataset complet beaucoup d'iteration
     //https://playground.tensorflow.org/#activation=sigmoid&batchSize=30&dataset=circle&regDataset=reg-plane&learningRate=0.1&regularizationRate=0&noise=0&networkShape=1,1&seed=0.12746&showTestData=false&discretize=false&percTrainData=90&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=regression&initZero=false&hideText=false
+//autre: erreur
+
 
 #if SETINGVERSION==0
     //#define DEBUG //à definir si on veux printf les message de debug attention à la saturation du shell
@@ -14,7 +17,7 @@
     #define DATATYPE    float //type de donné traité aprés normalisation float ou double
     #define EPSILLONE   FLT_MIN //Borne min du type de donée traité
     #define LEARNINGRATE 0.1 //pas d'aprentissage
-    #define FONCTION(x1) (x1) //équoition du modéle suposé inconus (frontierre de décision)
+    #define FONCTION(x1) (2*x1+1) //équoition du modéle suposé inconus (frontierre de décision)
     #define CRITERE(x1,x2) (x2>FONCTION(x1)) //critére de teste
     #define NBPARAM 2 //nombre de parametre d'entrée du modéle ici x1 et x2
     #define NBDATA  3000 //nombre de donné traité 
@@ -23,7 +26,7 @@
     #define NBENTREE {NBPARAM,1} //nombre de neurone par couche / nombre d'entré par neurone nbentre[C+1]=nbneurone[C]
     #define FCTACTIVATION {sigmoide} //fonction d'activation par couche
     #define PrintGradParam 0
-#else
+#elif SETINGVERSION==1
     #define DEBUG //à definir si on veux printf les message de debug attention à la saturation du shell
     //#define INITRDM  //la generation du dataset et des parametre sont soit aleatoir soit uniforme (pour NBPARAM=2)
     #define DATASETTYPE float //tipe du data set originel avant normalisation
@@ -41,4 +44,24 @@
     #define NBENTREE {NBPARAM,1,1} //nombre de neurone par couche / nombre d'entré par neurone nbentre[C+1]=nbneurone[C]
     #define FCTACTIVATION {sigmoide,sigmoide} //fonction d'activation par couche
     #define PrintGradParam 1
+#elif SETINGVERSION==2
+    //#define DEBUG //à definir si on veux printf les message de debug attention à la saturation du shell
+    #define INITRDM  //la generation du dataset et des parametre sont soit aleatoir soit uniforme (pour NBPARAM=2)
+    #define DATASETTYPE float //tipe du data set originel avant normalisation
+    #define BORNEMAX    100 //borne min du dataset generé
+    #define BORNEMIN    10//borne max du dataset generé
+    #define DATATYPE    float //type de donné traité aprés normalisation float ou double
+    #define EPSILLONE   FLT_MIN //Borne min du type de donée traité
+    #define LEARNINGRATE 0.1 //pas d'aprentissage
+    #define FONCTION(x1) (x1) //équoition du modéle suposé inconus (frontierre de décision)
+    #define CRITERE(x1,x2) (x2>FONCTION(x1)) //critére de teste
+    #define NBPARAM 2 //nombre de parametre d'entrée du modéle ici x1 et x2
+    #define NBDATA  3000 //nombre de donné traité 
+    #define NBITER  2000 //nombre d'iteration
+    #define NBCOUCHE 2 //nombre de couche du reseaux (2 minimum sinon utiliser la V3)
+    #define NBENTREE {NBPARAM,1,1} //nombre de neurone par couche / nombre d'entré par neurone nbentre[C+1]=nbneurone[C]
+    #define FCTACTIVATION {sigmoide,sigmoide} //fonction d'activation par couche
+    #define PrintGradParam 0
+#else
+    #error "SETINGVERSION non definie"
 #endif
